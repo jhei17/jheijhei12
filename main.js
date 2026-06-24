eaf6ba572dc1b40d81f70996f29fb8885fa20774
jhei17 <jhenifer.ochetski.nunes@escola.pr.gov.br>
2026-06-24 08:30:36 -0300

    ....

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {};}
+  botoes[i].onclick = function () { };
+}
 for (let i = 0; i < botoes.length; i++) {
   botoes[j].onclick = function () {
     botoes[i].classList.remove("ativo");
@@ -10,13 +11,15 @@ for (let i = 0; i < botoes.length; i++) {
 for (let j = 0; j < botoes.length; j++) {
   botoes[j].classList.remove("ativo");
 }
- botoes[i].classList.add("ativo");
-    textos[i].classList.add("ativo");
-  }
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
 
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-11-30");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
@@ -31,15 +34,15 @@ function calculaTempo(tempoObjetivo) {
   segundos %= 60;
   minutos %= 60;
   horas %= 24;
-  
+
   return (
-  dias +
-  " dias " +
-  horas +
-  " horas " +
-  minutos +
-  " minutos " +
-  segundos +
-  " segundos"
-);
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
 }
